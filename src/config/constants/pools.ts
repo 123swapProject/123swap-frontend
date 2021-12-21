792660819ccf1b7a3677402ff8220e614a5f3a4c
onetwothreeswap <onetwothreeswap@gmail.com>
2021-12-21 11:25:08 +0200

    Added test pool

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -2,20 +2,20 @@ import tokens from './tokens'
 import { PoolConfig, PoolCategory } from './types'
 
 const pools: PoolConfig[] = [
-    // {
-    // sousId: 0,
-    // stakingToken: tokens.cake,
-    // earningToken: tokens.cake,
-    // contractAddress: {
-    //   97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
-    //   56: '0x24539B16820ba44A9cc9569d11E51E599aB1371A',
-    // },
-    // poolCategory: PoolCategory.CORE,
-    // harvest: true,
-    // tokenPerBlock: '10',
-    // sortOrder: 1,
-    // isFinished: false,
-  // },
+    {
+    sousId: 0,
+    stakingToken: tokens.l23b,
+    earningToken: tokens.bnb,
+    contractAddress: {
+      97: '',
+      56: '0x24539b16820ba44A9cc9569D11E51E599ab1371b',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '1',
+    sortOrder: 1,
+    isFinished: true,
+  },
   {
     sousId: 1,
     stakingToken: tokens.l23b,
